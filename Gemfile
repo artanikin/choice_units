b62ac860d9847e76b28ea87091b185fc511a0702
Anikin Artyom <anikinartyom@gmail.com>
2013-07-14 22:15:33 +0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.12'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChoiceUnits::Application.config.secret_token = '6f77ee475628184ff810d0e9c51a8e37cd290bc80e23184e9c5cb03c6f711d65c832b177fe2f955da5ed23d3703c8877b4c34a707a79a0622af030ca6167bfb7'
